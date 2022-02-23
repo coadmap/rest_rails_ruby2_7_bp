14a8a494518a653948f19cf85296de01811e58f8
uchidataku <bbb.rchp99@gmail.com>
2022-02-23 16:12:24 +0900

    rubocop修正と不要なgemfile整理

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
+
 require 'active_support/core_ext/integer/time'
 
 Rails.application.configure do
+  ## Rails Default
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
@@ -14,7 +16,7 @@ Rails.application.configure do
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+        'Cache-Control' => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
@@ -22,11 +24,11 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  ## Mailer
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
 
-  config.active_storage.service = :local
-
+  ## Logger
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new($stdout)
     logger.formatter = config.log_formatter
